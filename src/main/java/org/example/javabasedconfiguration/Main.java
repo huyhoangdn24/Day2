316b207a575cb9be0228ed565d3f8e8cdbc90f4a
huyhoang <nguyenhuyhoangtp24@gmail.com>
2023-09-02 01:04:27 +0700

    update

--- src/main/java/org/example/javabasedconfiguration/Main.java
+++ src/main/java/org/example/javabasedconfiguration/Main.java
@@ -7,17 +7,12 @@ public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(BasedConfiguration.class);
         AccountService accountService = applicationContext.getBean(AccountService.class);
-
-        System.out.println("AccountService");
         System.out.println("Before money transfer");
         System.out.println("Account 1 balance: " + accountService.getAccount(1).getBalance());
         System.out.println("Account 2 balance: " + accountService.getAccount(2).getBalance());
-
         accountService.transferMoney(1, 2, 5.0);
         System.out.println("After money transfer");
         System.out.println("Account 1 balance: " + accountService.getAccount(1).getBalance());
         System.out.println("Account 2 balance: " + accountService.getAccount(2).getBalance());
-
-        applicationContext.close();
     }
 }
