843e4e54fa51b5f1059fed4e8c6647400df5c7eb
huyhoangdn24 <143186911+huyhoangdn24@users.noreply.github.com>
2023-09-02 01:01:49 +0700

    Update Main.java

--- src/main/java/org/example/javabasedconfiguration/Main.java
+++ src/main/java/org/example/javabasedconfiguration/Main.java
@@ -10,7 +10,6 @@ public class Main {
         System.out.println("Before money transfer");
         System.out.println("Account 1 balance: " + accountService.getAccount(1).getBalance());
         System.out.println("Account 2 balance: " + accountService.getAccount(2).getBalance());
-
         accountService.transferMoney(1, 2, 5.0);
         System.out.println("After money transfer");
         System.out.println("Account 1 balance: " + accountService.getAccount(1).getBalance());
